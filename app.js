f538ae5b1bad2d1746e6e6e6ff25e3efbf0e879e
satish <53410112+satish122@users.noreply.github.com>
2025-04-21 00:23:49 +0530

    add mongodb connection

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import  express  from "express";
-import dotenv from 'dotenv'
-
-const app=express()
-dotenv.config()
-
-const PORT="3000"
-app.listen(PORT,()=>{
-    console.log(`server running on port no ${PORT}`);
-})
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./database/connection.js";
+const app = express();
+dotenv.config();
+connectDB();
+const PORT = "3000";
+app.listen(PORT, () => {
+  console.log(`server running on port no ${PORT}`);
+});
