d63a0644bbdd7cc3addad2deef1e43c316177e88
satish <53410112+satish122@users.noreply.github.com>
2025-04-27 00:41:09 +0530

    sift back code

--- server/models/user.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  fullName: { type: String, required: true },
-  email: String,
-  password: String,
-});
-
-export const User = mongoose.model("User", userSchema);
